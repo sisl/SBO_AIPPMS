802e4eb9bd4ea4150d240c3573506ffafbbae16f
Joshua Ott <joshuaott2@gmail.com>
2022-09-13 09:39:29 -0700

    updating for import to SISL repo

--- InformationRockSample/AIPPMS/src/run_trials.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-NUM_PROCESSES = 30
-
-for i in range(NUM_PROCESSES):
-	subprocess.call(['julia', '--project', 'Trials.jl',str(1234*(i+1))])
--- InformationRockSample/AIPPMS/src/run_trials_script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for i in {1..30}; do
-	julia --project Trials.jl $((1234*i)) &
-done
